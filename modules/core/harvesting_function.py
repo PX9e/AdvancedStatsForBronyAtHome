d19475011386c939e42c06f921bca8456b864fb2
PX9e <PX9e@gmx.fr>
2014-03-24 16:45:36 +0100

    previous function renable

--- modules/core/harvesting_function.py
+++ modules/core/harvesting_function.py
@@ -19,32 +19,32 @@ def harvest_boinc_project(name, url, last_time_harvested):
         log_something_harvester(name, "TYPE_START", "STARTING ... ")
         start = time()
         log_something_harvester(name, "TYPE_INFO", "Downloading tables.xml... ")
-        #table_xml = download_file(url + "tables.xml", config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
-        #                                              "tables.xml")
+        table_xml = download_file(url + "tables.xml", config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
+                                                      "tables.xml")
         log_something_harvester(name, "TYPE_INFO", "Processing tables.xml... ")
-        #info_table_xml = db_tables_data_extraction("", table_xml)
-        if True:
-        #if (last_time_harvested and last_time_harvested < info_table_xml["last_update"]) or (not last_time_harvested):
+        info_table_xml = db_tables_data_extraction("", table_xml)
+
+        if (last_time_harvested and last_time_harvested < info_table_xml["last_update"]) or (not last_time_harvested):
             log_something_harvester(name, "TYPE_INFO", "Downloading db_dump.xml ... ")
-        #    dump_xml = download_file(url + "db_dump.xml", config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
-        #                                                  "db_dump.xml")
+            dump_xml = download_file(url + "db_dump.xml", config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
+                                                          "db_dump.xml")
             log_something_harvester(name, "TYPE_INFO", "Processing db_dump.xml ... ")
-        #    files_to_download = db_dump_data_extraction("", dump_xml)
+            files_to_download = db_dump_data_extraction("", dump_xml)
             log_something_harvester(name, "TYPE_INFO", "Downloading team file... ")
-        #    file_to_extract = download_file(url + files_to_download["team"]["file"] + ".gz",
-        #                                    config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
-        #                                    "team.gz")
+            file_to_extract = download_file(url + files_to_download["team"]["file"] + ".gz",
+                                            config["ASFBAH"]["CFG_SHARED_TMP_PATH"] + name + path.sep +
+                                            "team.gz")
             log_something_harvester(name, "TYPE_INFO", "Extracting ... ")
-        #    file_pr = decompression(file_to_extract, False)
+            file_pr = decompression(file_to_extract, False)
             log_something_harvester(name, "TYPE_INFO", "Looking for " + config["ASFBAH"]["TEAM"] + " data ... ")
-        #    try:
-        #        team = search_team_in_file_by_name_boinc(file_pr, config["ASFBAH"]["TEAM"])
-        #    except NoProjectException:
-        #        log_something_harvester(name, "TYPE_INFO", "No Team " + config["ASFBAH"]["TEAM"] + " into data ... ")
-        #        return None
+            try:
+                team = search_team_in_file_by_name_boinc(file_pr, config["ASFBAH"]["TEAM"])
+            except NoProjectException:
+                log_something_harvester(name, "TYPE_INFO", "No Team " + config["ASFBAH"]["TEAM"] + " into data ... ")
+                return None
             log_something_harvester(name, "TYPE_INFO", "Injecting into database ... ")
-        #    register_stats_state_in_database(team, name)
-        #    update_projects_harvest_time(name)
+            register_stats_state_in_database(team, name)
+            update_projects_harvest_time(name)
             elapsed = (time() - start)
             log_something_harvester(name, "TYPE_COMPLETE", "Complete in " + str(round(elapsed, 3)) + " sec")
         else:
